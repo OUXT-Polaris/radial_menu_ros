355f01fa374b45ffe669fec9e0411dc5c8f56977
Masaya Kataoka <ms.kataoka@gmail.com>
2020-09-06 17:21:52 +0900

    enable load as component
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ radial_menu_backend/src/radial_menu_backend_component.cpp
@@ -0,0 +1,4 @@
+#include <radial_menu_backend/backend_component.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+
+RCLCPP_COMPONENTS_REGISTER_NODE(radial_menu_backend::BackendComponent)
--- radial_menu_backend/src/radial_menu_backend_plugins.cpp
+++
@@ -1,3 +0,0 @@
-#include <radial_menu_backend/backend_component.hpp>
-
-//PLUGINLIB_EXPORT_CLASS(radial_menu_backend::BackendNodelet, nodelet::Nodelet);
